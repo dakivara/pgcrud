96f64dc3b46d0367e51c5f12472d8ecbb50010e1
Danijel Kivaranovic <danijel@kivaranovic.com>
2024-11-23 13:39:41 +0100

    implement __str__ method

--- pgcrud/undefined.py
+++ pgcrud/undefined.py
@@ -10,6 +10,12 @@ class UndefinedType(type):
     def __call__(cls):
         raise TypeError("'Undefined' object is not callable")
 
+    def __str__(cls) -> str:
+        return 'Undefined'
+
+    def __repr__(cls) -> str:
+        return cls.__str__()
+
 
 class Undefined(metaclass=UndefinedType):
     pass
