0dd3df365388dc5eb3b5991accd805d7c372300a
Danijel Kivaranovic <danijel@kivaranovic.com>
2024-12-04 23:21:49 +0100

    add more test cases

--- tests/test_get_one.py
+++ tests/test_get_one.py
@@ -40,3 +40,30 @@ def test_get_customer_by_invalid_id(cursor):
     )
 
     assert customer is None
+
+
+def test_get_customer_id_by_name(cursor):
+
+    customer_id = pg.get_one(
+        cursor=cursor,
+        select=e.id,
+        from_=e.customer,
+        where=e.name == 'Customer A',
+    )
+
+    assert isinstance(customer_id, int)
+    assert customer_id == 1
+
+
+def test_get_customer_tuple_by_id(cursor):
+
+    customer_tuple = pg.get_one(
+        cursor=cursor,
+        select=(e.id, e.name),
+        from_=e.customer,
+        where=e.id == 1,
+    )
+
+    assert isinstance(customer_tuple, tuple)
+    assert customer_tuple[0] == 1
+    assert customer_tuple[1] == 'Customer A'
