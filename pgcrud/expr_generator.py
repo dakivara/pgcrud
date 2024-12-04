662f17c489389cc94d65c888ee58a733305804c0
Danijel Kivaranovic <danijel@kivaranovic.com>
2024-12-04 13:20:43 +0100

    remove frame boundaries from generator

--- pgcrud/expr_generator.py
+++ pgcrud/expr_generator.py
@@ -3,8 +3,6 @@ from pgcrud.expr import ReferenceExpr
 
 __all__ = ['ExprGenerator']
 
-from pgcrud.frame_boundaries import CurrentRow, UnboundedFollowing, UnboundedPreceding
-
 
 class ExprGeneratorType(type):
 
@@ -13,18 +11,6 @@ class ExprGeneratorType(type):
 
     def __call__(cls, name: str) -> ReferenceExpr:
         return ReferenceExpr(name)
-
-    @property
-    def UNBOUNDED_PRECEDING(cls) -> UnboundedPreceding:
-        return UnboundedPreceding()
-
-    @property
-    def UNBOUNDED_FOLLOWING(cls) -> UnboundedFollowing:
-        return UnboundedFollowing()
-
-    @property
-    def CURRENT_ROW(cls) -> CurrentRow:
-        return CurrentRow()
 
 
 class ExprGenerator(metaclass=ExprGeneratorType):
